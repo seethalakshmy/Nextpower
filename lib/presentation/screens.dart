cb9619494457e4be5d29666a920ff3e4bc80deaf
devumani <devumani10@gmail.com>
2023-03-27 16:47:44 +0100

    error changes client mentioned

--- lib/presentation/screens.dart
+++ lib/presentation/screens.dart
@@ -8,6 +8,6 @@ export 'package:project/presentation/my_vehicles/my_vehicles.screen.dart';
 export 'package:project/presentation/otp/otp.screen.dart';
 export 'package:project/presentation/profile/profile.screen.dart';
 export 'package:project/presentation/registration/registration.screen.dart';
-export 'package:project/presentation/splash_screen/splash_screen.screen.dart';
+export 'package:project/presentation/splash_screen/splash.screen.dart';
 export 'package:project/presentation/station.details/station_details.screen.dart';
 export 'package:project/presentation/station_list/station_list.screen.dart';
