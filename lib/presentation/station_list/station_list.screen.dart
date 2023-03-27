4ee0f79641ff30a72d835794763d8370ed9b9778
devumani <devumani10@gmail.com>
2023-03-27 16:38:17 +0100

    popup completed

--- lib/presentation/station_list/station_list.screen.dart
+++ lib/presentation/station_list/station_list.screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:project/infrastructure/navigation/navigation_utils.dart';
+import 'package:project/infrastructure/widgets/appbar/custom_appbar.dart';
 import 'package:project/infrastructure/widgets/loaders/loading_widget.dart';
 import 'package:project/presentation/station.details/widgets/station_brief_details_widget.dart';
 
@@ -12,36 +13,34 @@ class StationListScreen extends GetView<StationListController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Stations'),
-        centerTitle: true,
+      appBar: CustomAppbar(
+        title: 'Stations',
       ),
-      body: Obx(() =>
-      controller.isLoading.value
+      body: Obx(() => controller.isLoading.value
           ? const LoadingWidget()
           : ListView.builder(
-          itemCount: controller.stationList.length,
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () {
-                NavigationUtils().callStationDetails();
-              },
-              onLongPress: () {
-                Get.dialog(StationBriefDetailsWidget(
-                    controller.stationList[index]));
-              },
-              child: Card(
-                margin: const EdgeInsets.all(10),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    controller.stationList[index].stationName ?? "",
-                    style: const TextStyle(fontWeight: FontWeight.bold),
+              itemCount: controller.stationList.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    NavigationUtils().callStationDetails();
+                  },
+                  onLongPress: () {
+                    Get.dialog(StationBriefDetailsWidget(
+                        controller.stationList[index]));
+                  },
+                  child: Card(
+                    margin: const EdgeInsets.all(10),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        controller.stationList[index].stationName ?? "",
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            );
-          })),
+                );
+              })),
     );
   }
 }
